556e6953813915b413ebbc44e2649039df0172f5
Zi_Yan416 <Zi_Yan416@163.com>
2024-05-22 20:11:41 +0800

    上传了修改后的网页代码以匹配github

--- js/login.js
+++ js/login.js
@@ -24,7 +24,7 @@ document.getElementById("submit").addEventListener("click", function(event) {
 			});
 			if (account) {
 				alert("登录成功");
-				window.location.href = "home.html";
+				window.location.href = "html/home.html";
 			} else {
 				alert("用户名或密码错误");
 			}
